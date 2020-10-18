a22aa51b4c57f842efb802247c3ffced84cf7c96
CodeLegend27 <65169334+CodeLegend27@users.noreply.github.com>
2020-10-18 19:57:18 +0200

    initial upload
    finished - no sounds / background music / animations

---
+++ main.js
@@ -0,0 +1,39 @@
+// 
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    alert("Please Play on Desktop PC")
+   }
+
+   // IMG ARRAY
+
+var images = ['dino8.png', 'dino1.png', 'dino2.png','dino3.png', 'dino4.png', 'dino5.png', 'dino6.png', 'dino7.png']; 
+
+$( document ).ready(function() {
+  
+// POPULATE SITE WITH IMAGES -- Randomly, Random size
+$('.square').each(function() {
+    var randNumb =  Math.floor(Math.random() * 71) + 30;
+    var rand = Math.floor(Math.random() * images.length);
+    $(this).append('<img class="dino" src="img/' + images[rand] + '"/ width="'+ randNumb +'%" height="'+randNumb+'%">');
+   });
+
+   // COUNT TILL GAME FINISH, Show Egg and smiley, RELOAD PAGE
+let x = 0;
+
+
+$('.dino').hover(function(){
+   
+    $(this).remove();
+        x++
+    if (x == 50){
+        $("#egg").css("display", "block");
+        $("p").css("display", "block");
+
+        setTimeout(location.reload.bind(location), 8000);
+    }
+    console.log(x);
+});
+
+
+
+}); 
+
